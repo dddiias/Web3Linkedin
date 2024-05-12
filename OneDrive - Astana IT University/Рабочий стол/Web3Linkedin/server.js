b89ca9608b544fdc7456d5873341baf8aa551729
Dias Salakhutdinov <220182@astanait.edu.kz>
2024-05-13 01:41:52 +0600

    Add server and connect wallet pages

---
+++ OneDrive - Astana IT University/Рабочий стол/Web3Linkedin/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("connect_wallet");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
